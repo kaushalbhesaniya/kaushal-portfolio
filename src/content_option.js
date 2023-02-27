62d7cc19a71ac413f4d71a172ef4f1e655926f19
kaushal <kaushalbhesaniya1212@gmail.com>
2023-02-27 17:42:34 +0530

    done

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "KAUSHAL";
+const meta = {
+    title: "Kaushal Patel",
+    description: "I’m Kaushal Patel Full stack devloper,currently working in India",
+};
+
+const introdata = {
+    title: "I’m Kaushal Patel",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Api",
+    },
+    description: "In my experience, requirements change quite often, or new situations will arise that weren’t anticipated at the start of the project. If the situation can be addressed with a plugin, I just whip open the standalone plugin page, make the updates and pop the new plugin back in. Because the plugin is self-contained, it’s easy to recreate the problem, fix it, and get it back into the codebase.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hardworking with an ability to learn new things quickl. Tendency to complete the targets within the given time limits. Simplicity is prerequisite for reliability"
+};
+const worktimeline = [{
+    jobtitle: "Web Design ",
+    where: "HK Technolab",
+    date: "2019",
+},
+{
+    jobtitle: "JavaScript",
+    where: "HK Technolab",
+    date: "2019",
+},
+{
+    jobtitle: "React js - Node Js - Full-Stack Developer",
+    where: "Silico Software Solution",
+    date: "2022",
+},
+];
+
+const skills = [{
+    name: "Web design(HTML - CSS - BOOSTRAP)",
+    value: 90,
+},
+{
+    name: "JQUERY",
+    value: 85,
+},
+{
+    name: "JAVASCRIPT",
+    value: 95,
+},
+{
+    name: "REACT",
+    value: 97,
+},
+{
+    name: "NODE",
+    value: 98,
+},
+];
+
+const services = [{
+    title: "Web Design",
+    description: "What separates design from art is that design is meant to be... functional.",
+},
+{
+    title: "React js",
+    description: "All you need in this life is ignorance and confidence, and then success is sure",
+},
+{
+    title: "Node js ",
+    description: "Reusability is key in reducing bugs and coding quickly. The more I use a piece of code, the more confident and familiar I become with it, which in turn significantly speeds up my development time.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kaushalbhesaniya1212@gmail.com",
+    YOUR_FONE: "8160536027",
+    description: "22,Ruxmani Society,Near Tapi Darshan Society,Nana Varachha,Surat,Gujarat,India",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/kaushalbhesaniya",
+    facebook: "https://www.facebook.com/profile.php?id=100009154121985",
+    linkedin: "https://www.linkedin.com/in/kaushal-bhesaniya-a1a85618a/",
+
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
